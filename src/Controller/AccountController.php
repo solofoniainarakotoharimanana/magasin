3c16fccccd470d3e2d36b86e7dd996c5d963c064
solofoniaina <solofoniainarakotoharimanana@gmail.com>
2024-05-10 15:09:15 +0300

    Feat: Fix bug in login

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/account')]
+class AccountController extends AbstractController
+{
+    #[Route('/', name: 'app.account')]
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
